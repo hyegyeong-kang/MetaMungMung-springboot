107465fcb0c792bc354f64a3d5749d5a6366a5d0
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-10 23:41:30 +0900

    initial commit

---
+++ src/main/java/com/metanet/metamungmung/MetaMungMungApplication.java
@@ -0,0 +1,13 @@
+package com.metanet.metamungmung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaMungMungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaMungMungApplication.class, args);
+    }
+
+}
