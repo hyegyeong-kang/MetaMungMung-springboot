889d1ab5c030a0d124f40f49f2a86895c5c54fcd
jeonga-Son <jjeong0106@gmail.com>
2023-04-16 17:45:33 +0900

    add getOffMeetingList feature

--- src/main/java/com/metanet/metamungmung/controller/meeting/OffMeetingController.java
+++ src/main/java/com/metanet/metamungmung/controller/meeting/OffMeetingController.java
@@ -3,7 +3,6 @@ package com.metanet.metamungmung.controller.meeting;
 import com.metanet.metamungmung.dto.meeting.OffMeetingDTO;
 import com.metanet.metamungmung.service.meeting.OffMeetingService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
