a99b8b5aca67b5ee8c9e9d727f9027e29e1dbfec
lhyeonj120 <isk04387@gmail.com>
2023-04-18 00:31:24 +0900

    chore: update OnMeetingMapperTests file

--- src/test/java/com/metanet/metamungmung/mapper/meeting/OnMeetingMapperTests.java
+++ src/test/java/com/metanet/metamungmung/mapper/meeting/OnMeetingMapperTests.java
@@ -2,13 +2,16 @@ package com.metanet.metamungmung.mapper.meeting;
 
 
 import com.metanet.metamungmung.dto.meeting.OnMeetingDTO;
-import lombok.extern.log4j.Log4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
 public class OnMeetingMapperTests {
 
+    @Autowired
     private OnMeetingMapper onMeetingMapper;
 
     @Test
@@ -18,6 +21,15 @@ public class OnMeetingMapperTests {
         dto.setCategory("소형견");
         dto.setIsPublic("0");
         onMeetingMapper.createOnMeeting(dto);
+
+        assertThat(dto.getOnMeetingIdx()).isNotNull();
+    }
+
+    @Test
+    public void getOnMeetingByIdTest(){
+        OnMeetingDTO dto = onMeetingMapper.getOnMeetingById(1L);
+
+        assertThat(dto.getOnMeetingIdx()).isEqualTo(1L);
     }
 
 }
