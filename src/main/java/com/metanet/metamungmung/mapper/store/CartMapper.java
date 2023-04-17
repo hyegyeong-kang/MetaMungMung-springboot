eeeb8645bd3f58eced3e19b6eaea0d311230b7dd
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-17 10:40:29 +0900

    hold other methods

--- src/main/java/com/metanet/metamungmung/mapper/store/CartMapper.java
+++ src/main/java/com/metanet/metamungmung/mapper/store/CartMapper.java
@@ -1,6 +1,7 @@
 package com.metanet.metamungmung.mapper.store;
 
 import com.metanet.metamungmung.dto.store.CartDTO;
+import com.metanet.metamungmung.dto.store.CartProductDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,23 +13,23 @@ public interface CartMapper {
         List<CartDTO> getMyCartList(Long m_id);
 
         // 해당 회원 장바구니 물건추가
-        public void addCart(CartDTO cart);
+     //   public void addCart(CartProductDTO cartProduct);
 
-        // 장바구니 중복 상품 확인
-        public int checkCart(@Param("p_id")Long p_id, @Param("m_id")Long m_id);
-
-        // 중복된 상품이 있다면 넣지말고 수량 더해주기
-        public void updateCount(CartDTO cart);
-
-        // 장바구니 물건 삭제
-        public void deleteCart(@Param("p_id")Long p_id, @Param("m_id")Long m_id);
-
-        // 장바구니 전체 비우기
-        public void deleteAllCart(Long m_id);
-
-        // 장바구니 물건 업데이트 (장바구니에서 수량변경하는 것)
-        public void updateCart(@Param("p_id")Long p_id, @Param("m_id")Long m_id, @Param("quantity")int quantity);
-
-        // 해당 회원의 장바구니 전체 금액 출력
-        public Long getTotalPrice(Long m_id);
+//        // 장바구니 중복 상품 확인
+//        public int checkCart(@Param("p_id")Long p_id, @Param("m_id")Long m_id);
+//
+//        // 중복된 상품이 있다면 넣지말고 수량 더해주기
+//        public void updateCount(CartDTO cart);
+//
+//        // 장바구니 물건 삭제
+//        public void deleteCart(@Param("p_id")Long p_id, @Param("m_id")Long m_id);
+//
+//        // 장바구니 전체 비우기
+//        public void deleteAllCart(Long m_id);
+//
+//        // 장바구니 물건 업데이트 (장바구니에서 수량변경하는 것)
+//        public void updateCart(@Param("p_id")Long p_id, @Param("m_id")Long m_id, @Param("quantity")int quantity);
+//
+//        // 해당 회원의 장바구니 전체 금액 출력
+//        public Long getTotalPrice(Long m_id);
 }
