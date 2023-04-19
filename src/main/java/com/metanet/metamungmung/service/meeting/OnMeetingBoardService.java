03daa2dc3d177ff1c7ded94b819ab3a99393fa9c
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-19 15:58:37 +0900

    update onMeetingBoard files
    working....

--- src/main/java/com/metanet/metamungmung/service/meeting/OnMeetingBoardService.java
+++ src/main/java/com/metanet/metamungmung/service/meeting/OnMeetingBoardService.java
@@ -1,5 +1,26 @@
 package com.metanet.metamungmung.service.meeting;
 
+import com.metanet.metamungmung.dto.meeting.OnMeetingBoardDTO;
+import com.metanet.metamungmung.dto.meeting.OnMeetingMemDTO;
+
+import java.util.List;
+
 public interface OnMeetingBoardService {
+
+    // 게시글 목록 조회
+    public List<OnMeetingBoardDTO> getBoardList(Long onMeetingIdx);
+
+    // 온모임 가입한 사용자 리스트
+    public List<OnMeetingMemDTO> getOnMeetingMembers(Long onMeetingIdx);
+
+    // 게시글 상세 조회
+
+    // 게시글 작성
+
+    // 게시글 수정
+
+    // 게시글 삭제
+
+    // 게시글 검색
 
 }
