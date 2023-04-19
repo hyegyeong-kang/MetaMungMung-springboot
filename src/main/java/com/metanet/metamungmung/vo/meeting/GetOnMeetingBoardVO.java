17941bbfa1cca3c2c925e54c1e6cd6f11d1442cd
hyegyeong-kang <rkdgprud96@gmail.com>
2023-04-19 22:53:55 +0900

    add VO file

--- src/main/java/com/metanet/metamungmung/vo/meeting/GetOnMeetingBoardVO.java
+++ src/main/java/com/metanet/metamungmung/vo/meeting/GetOnMeetingBoardVO.java
@@ -1,10 +1,17 @@
 package com.metanet.metamungmung.vo.meeting;
 
+import com.metanet.metamungmung.dto.meeting.OnMeetingBoardReplyDTO;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class GetOnMeetingBoardVO {
-    private GetOnMeetingMemVO onMeetingMember;
+    private Long onMeetingBoardIdx;
+    private Long onMeetingMemIdx;
+    private Long onMeetingIdx;
+    private Long memberIdx;
+    private List<OnMeetingBoardReplyDTO> replyList;
+
+
 }
