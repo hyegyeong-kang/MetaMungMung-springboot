458af2eab9393647eb6f94c59d8e59143ecb5fec
yebin shin <pandayb1016@gmail.com>
2023-04-20 09:51:20 +0900

    chore: security files

--- src/main/java/com/metanet/metamungmung/security/AuthenticationFilter.java
+++ src/main/java/com/metanet/metamungmung/security/AuthenticationFilter.java
@@ -2,7 +2,7 @@ package com.metanet.metamungmung.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.metanet.metamungmung.dto.member.MemberDTO;
-import com.metanet.metamungmung.mapper.member.MemberMapper;
+import com.metanet.metamungmung.service.member.MemberService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
@@ -26,13 +26,13 @@ import java.util.Date;
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Autowired
-    private MemberMapper memberMapper;
+    private MemberService memberService;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public AuthenticationFilter(AuthenticationManager authenticationManager, MemberMapper memberMapper) {
+    public AuthenticationFilter(AuthenticationManager authenticationManager, MemberService memberService) {
         super.setAuthenticationManager(authenticationManager);
-        this.memberMapper = memberMapper;
+        this.memberService = memberService;
         setFilterProcessesUrl("/members/login");
     }
 
@@ -40,8 +40,10 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
             MemberDTO memberDTO = new ObjectMapper().readValue(request.getInputStream(), MemberDTO.class);
-
+            logger.info("-----------------"+memberDTO.getMemberId());
+            logger.info("-----------------"+memberDTO.getPassword());
             if(memberDTO != null) {
+                logger.info("-------------------확인--------------");
                 return getAuthenticationManager().authenticate(
                         new UsernamePasswordAuthenticationToken(memberDTO.getMemberId(), memberDTO.getPassword(), new ArrayList<>())
                 );
@@ -56,15 +58,16 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        System.out.println("여기가 넘어와야 해요--------------------------");
         MemberDTO member = (MemberDTO) authResult.getPrincipal();
         String username = member.getUsername();
+        response.addHeader("memberIdx",member.getMemberIdx().toString());
         String token = Jwts.builder()
-                        .setHeaderParam("type", "token")
-                        .setSubject(username)
-                        .claim("memberIdx", member.getMemberIdx())
-                        .setExpiration(new Date(System.currentTimeMillis() +1*(1000*60*60*24*365)))
-                        .signWith(SignatureAlgorithm.HS512, "secret")
-                        .compact();
+                .setHeaderParam("type", "token")
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() +1*(1000*60*60*24*365)))
+                .signWith(SignatureAlgorithm.HS512, "secret")
+                .compact();
 
         response.addHeader("token", token);
     }
--- src/main/java/com/metanet/metamungmung/security/JwtFilter.java
+++ src/main/java/com/metanet/metamungmung/security/JwtFilter.java
@@ -1,19 +1,14 @@
 package com.metanet.metamungmung.security;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.metanet.metamungmung.dto.member.MemberDTO;
-import com.metanet.metamungmung.mapper.member.MemberMapper;
+import com.metanet.metamungmung.service.member.MemberService;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -21,54 +16,49 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Map;
-
 
 public class JwtFilter extends BasicAuthenticationFilter {
 
-    private MemberMapper memberMapper;
+    @Autowired
+    private MemberService memberService;
 
-    public JwtFilter(AuthenticationManager authenticationManager, MemberMapper memberMapper) {
+    public JwtFilter(AuthenticationManager authenticationManager, MemberService memberService) {
         super(authenticationManager);
-        this.memberMapper = memberMapper;
+        this.memberService = memberService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request2, HttpServletResponse response2, FilterChain filterChain) throws ServletException, IOException {
         HttpServletRequest rq = (HttpServletRequest) request2;
         HttpServletResponse rp = (HttpServletResponse) response2;
+        System.out.println("--------------------------혹시 시작도 안 되는 거니?");
         if (rq.getHeader("AUTHORIZATION") == null) {
+            System.out.println("--------------------------혹시 없는거니?");
             return;
         } else {
             String authorizationHeader = rq.getHeader("AUTHORIZATION");
             String jwt = authorizationHeader.replace("Bearer", "");
+            System.out.println("--------------------------나오니?");
             if (!isJwtValid((jwt))) {
                 return;
             }
-            String name = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).getBody().getSubject();
-            MemberDTO memberDTO = memberMapper.findByUserId(name);
+            // jwt에서 username 빼옴
+            // loadUserByUsername을 이용
+            String username = Jwts.parser().setSigningKey("secret").parseClaimsJws(jwt).getBody().getSubject();
+            UserDetails userDetails = memberService.loadUserByUsername(username);
 
-            if(memberDTO != null) {
-                UserDetails user = User.builder()
-                        .username(memberDTO.getMemberId())
-                        .password(memberDTO.getPassword())
-                        .authorities(new SimpleGrantedAuthority(memberDTO.getAuthority()))
-                        .build();
-
-                Authentication authentication = new UsernamePasswordAuthenticationToken(
-                        user,
-                        memberDTO.getPassword(),
-                        Collections.singleton(new SimpleGrantedAuthority(memberDTO.getAuthority())));
+            if(userDetails != null) {
+                UsernamePasswordAuthenticationToken  authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                if (authentication != null && authentication.isAuthenticated() && authentication.getAuthorities().stream()
-                        .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_MEMBER"))) {
-                    filterChain.doFilter(request2, response2);
-                }
-                else {
-                    response2.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                }
+
+                System.out.println("ddddddddddddddddddddddddddddddd" + authentication);
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request2));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                filterChain.doFilter(request2, response2);
+
             } else {
                 throw new UsernameNotFoundException("Member not found");
             }
@@ -89,24 +79,4 @@ public class JwtFilter extends BasicAuthenticationFilter {
         }
         return returnValue;
     }
-
-    public long getMemberIdxFromToken(String token) {
-        String[] tokenArr = token.split("\\.");
-        String payload = tokenArr[1];
-        Base64.Decoder decoder = Base64.getUrlDecoder();
-        String decodedPayload = new String(decoder.decode(payload));
-
-        // JSON 형태의 문자열을 Map으로 변환합니다.
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> payloadMap = null;
-        try {
-            payloadMap = objectMapper.readValue(decodedPayload, new TypeReference<Map<String, Object>>(){});
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        // "memberIdx" 키의 값을 Long 타입으로 반환합니다.
-        return ((Number)payloadMap.get("memberIdx")).longValue();
-    }
-
 }
