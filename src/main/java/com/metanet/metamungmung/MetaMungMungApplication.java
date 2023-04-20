eab4356328a6d4921f6561afa545f53c72f973b8
lhyeonj120 <isk04387@gmail.com>
2023-04-21 02:39:45 +0900

    add aop

--- src/main/java/com/metanet/metamungmung/MetaMungMungApplication.java
+++ src/main/java/com/metanet/metamungmung/MetaMungMungApplication.java
@@ -2,8 +2,10 @@ package com.metanet.metamungmung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class MetaMungMungApplication {
 
     public static void main(String[] args) {
